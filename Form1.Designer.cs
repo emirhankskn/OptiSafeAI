8e9d90e044b231ba1d9f655b857a862a31df3747
seldean <sertanbalta44@gmail.com>
2024-12-03 19:27:45 +0300

    Test Commit
    Test Commitment, only two text properties changed.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,18 +39,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(239, 69);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Login:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(239, 153);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Password:";
             // 
             // textBox1
             // 
